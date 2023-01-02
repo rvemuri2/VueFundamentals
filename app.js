1cd6c089a5f1c247f683d69547bc48a78a8663ab
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-01 16:56:09 -0800

    Multiple Instances

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ Vue.createApp({
   data() {
     return {
       firstName: "John",
+      lastName: "Smith",
     };
   },
 }).mount("#app");
+
+Vue.createApp({
+  data() {
+    return {
+      firstName: "Jane",
+      lastName: "Doe",
+    };
+  },
+}).mount("#app2");
