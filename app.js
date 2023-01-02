0d149f8b599aadad2c4bd01a311e320792f6e9b9
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-01 17:25:36 -0800

    Outputting raw HTML

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const vm = Vue.createApp({
       firstName: "John",
       lastName: "Smith",
       url: "https://google.com",
+      raw_url:
+        '<a href = "https://www.google.com" target = "_blank">Google</a>',
     };
   },
   methods: {
